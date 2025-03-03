4fda071d7ef271121c23ea3e01778b78478c18ff
Jason Smith <jasondsmith2501@gmail.com>
2025-03-03 10:45:26 -0800

    Updated outer space to new schema

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,29 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
-(
+CREATE TABLE galaxy (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  name VARCHAR(50)
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+CREATE TABLE star (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+CREATE TABLE planet (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(36) NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  galaxyid INTEGER NOT NULL,
+  orbitStarId INTEGER NOT NULL,
+  FOREIGN KEY (galaxyid) REFERENCES galaxy(id),
+  FOREIGN KEY (orbitStarId) REFERENCES star(id)
+);
+
+CREATE TABLE moon (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  planetid INTEGER NOT NULL,
+  FOREIGN KEY (planetid) REFERENCES planet(id)
+);
