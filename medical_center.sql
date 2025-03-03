fa4b51449290853ba2232558d6a24a2fa4272f22
Jason Smith <jasondsmith2501@gmail.com>
2025-03-03 12:51:42 -0800

    Fixed schemas based on feedback from solution

--- medical_center.sql
+++ medical_center.sql
@@ -4,17 +4,11 @@ CREATE DATABASE medicalcenter;
 
 \c medicalcenter
 
-CREATE TABLE medicalCenter (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
-
 CREATE TABLE doctor (
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
-    medicalID INTEGER NOT NULL,
-    FOREIGN KEY (medicalID) REFERENCES medicalCenter(id)
+    medicalID INTEGER NOT NULL
 );
 
 CREATE TABLE patient (
@@ -23,24 +17,24 @@ CREATE TABLE patient (
     lastName VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE doctor_patient (
-    doctorid INTEGER,
-    patientid INTEGER,
-    PRIMARY KEY (doctorid, patientid),
-    FOREIGN KEY (doctorid) REFERENCES doctor(id),
-    FOREIGN KEY (patientid) REFERENCES patient(id)
-);
-
 CREATE TABLE visit (
     id SERIAL PRIMARY KEY,
     patientid INTEGER NOT NULL,
+    doctorid INTEGER NOT NULL,
     visitDate DATE,
-    FOREIGN KEY (patientid) REFERENCES patient(id)
+    FOREIGN KEY (patientid) REFERENCES patient(id),
+    FOREIGN KEY (doctorid) REFERENCES doctor(id)
 );
 
 CREATE TABLE disease (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(50)
+);
+
+CREATE TABLE diagnosis (
+    id SERIAL PRIMARY KEY,
     visitid INTEGER NOT NULL,
-    FOREIGN KEY (visitid) REFERENCES visit(id)
+    diseaseid INTEGER NOT NULL,
+    FOREIGN KEY (visitid) REFERENCES visit(id),
+    FOREIGN KEY (diseaseid) REFERENCES disease(id)
 );
--- soccer_league.sql
+++ soccer_league.sql
@@ -4,18 +4,27 @@ CREATE DATABASE soccerleague;
 
 \c soccerleague
 
-CREATE TABLE league (
+-- Same
+CREATE TABLE season (
+    id SERIAL PRIMARY KEY,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL
+);
+
+-- DIff on rank
+CREATE TABLE team (
+    id SERIAL PRIMARY KEY,
+    leagueid INTEGER NOT NULL,
+    rank INTEGER NOT NULL
+);
+
+-- same
+CREATE TABLE referee (
     id SERIAL PRIMARY KEY,
     name VARCHAR(36)
 );
 
-CREATE TABLE team (
-    id SERIAL PRIMARY KEY,
-    leagueid INTEGER NOT NULL,
-    rank INTEGER NOT NULL,
-    FOREIGN KEY (leagueid) REFERENCES league(id)
-);
-
+-- close enough
 CREATE TABLE player (
     id SERIAL PRIMARY KEY,
     teamid INTEGER NOT NULL,
@@ -23,19 +32,29 @@ CREATE TABLE player (
     FOREIGN KEY (teamid) REFERENCES team(id)
 );
 
-CREATE TABLE referee (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(36)
-);
-
+-- close enough
 CREATE TABLE match (
     id SERIAL PRIMARY KEY,
     team1id INTEGER NOT NULL,
     team2id INTEGER NOT NULL,
+    seasonid INTEGER NOT NULL,
     FOREIGN KEY (team1id) REFERENCES team(id),
-    FOREIGN KEY (team2id) REFERENCES team(id)  
+    FOREIGN KEY (team2id) REFERENCES team(id),
+    FOREIGN KEY (seasonid) REFERENCES season(id)
 );
 
+CREATE TYPE match_result AS ENUM ('Win', 'Loss', 'Draw');
+
+CREATE TABLE result (
+    id SERIAL PRIMARY KEY,
+    matchid INTEGER NOT NULL,
+    teamid INTEGER NOT NULL,
+    result match_result NOT NULL,
+    FOREIGN KEY (matchid) REFERENCES match(id),
+    FOREIGN KEY (teamid) REFERENCES team(id)
+);
+
+-- Different, but I like mine better
 CREATE TABLE referee_match (
     matchid INTEGER NOT NULL,
     refereeid INTEGER NOT NULL,
@@ -44,14 +63,7 @@ CREATE TABLE referee_match (
     FOREIGN KEY (refereeid) REFERENCES referee(id)
 );
 
-CREATE TABLE season (
-    id SERIAL PRIMARY KEY,
-    startDate DATE NOT NULL,
-    endDate DATE NOT NULL,
-    leagueid INTEGER NOT NULL,
-    FOREIGN KEY (leagueid) REFERENCES league(id)
-);
-
+-- Same
 CREATE TABLE goal (
     id SERIAL PRIMARY KEY,
     playerid INTEGER NOT NULL,
